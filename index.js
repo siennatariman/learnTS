3966546b2950dae629380719009f49b231c35350
siennatariman <tariman.sienna@gmail.com>
2024-01-04 19:25:09 +0800

    primitive types

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var sport = 'football';
 var id = 5;
+// Primitive types (not an object, has no methods)
+// 7 types = string, number, biginit, boolean, undefined, null, symbol
+// Are immutable = cannot be altered  
+// Ex.
+var name = 'Danny';
+name.toLowerCase();
+console.log(name); // Danny - the  string method did not mutate the string
+var arr = [1, 3, 5, 7];
+arr.pop();
+console.log(arr); // [1,3,5] the array method mutated the array
+name = 'Anna'; // Assignment gives the primitive a new (not mutated) value
+console.log(name);
--- index.ts
+++ index.ts
@@ -2,3 +2,19 @@ let sport = 'football';
 
 let id = 5;
 
+// Primitive types (not an object, has no methods)
+	// 7 types = string, number, biginit, boolean, undefined, null, symbol
+	
+	// Are immutable = cannot be altered  
+
+// Ex.
+let name =  'Danny';
+name.toLowerCase();
+console.log(name);  // Danny - the  string method did not mutate the string
+
+let arr = [1,3,5,7];
+arr.pop();
+console.log(arr); // [1,3,5] the array method mutated the array
+
+name = 'Anna'; // Assignment gives the primitive a new (not mutated) value
+console.log(name);
