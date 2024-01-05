5d1271c7c412f793c09e792247cbf90f4984712c
siennatariman <tariman.sienna@gmail.com>
2024-01-05 18:14:37 +0800

    arrays

--- index.js
+++ index.js
@@ -26,3 +26,13 @@ unit = 8;
 var age;
 age = 26;
 age = '26';
+// Arrays
+// the type of data an array can contain can be defined in TS
+// Ex.
+var ids = [1, 2, 3, 4, 5]; // can only contain numbers
+var names = ['Danny', 'Anna', 'Bazza']; // can only contain strings
+var options = [true, false, false]; // can only contain true or false
+var books = [{ name: 'Fooled by randomness', author: 'Nassim Taleb' }, { name: 'Sapiens', author: 'Yuval Noah Harari' },]; // can only contain objects
+var arr = ['hello', 1, true]; // any reverts TS back to JS
+ids.push(6);
+ids.push('7'); // ERROR: Argument of  type 'string' is not assignable to parameter of type  'numeber'
--- index.ts
+++ index.ts
@@ -43,3 +43,19 @@ let age: string | number;
 age = 26;
 age = '26';
 
+
+// Arrays
+	// the type of data an array can contain can be defined in TS
+
+// Ex.
+let ids: number[] = [1,2,3,4,5]; // can only contain numbers
+let names: string[] = ['Danny', 'Anna',  'Bazza']; // can only contain strings
+let options: boolean[] = [true, false, false]; // can only contain true or false
+let books: object[] = [{name: 'Fooled by randomness', author: 'Nassim Taleb'}, {name: 'Sapiens', author: 'Yuval Noah Harari'},]; // can only contain objects
+
+let arr: any[]  = ['hello', 1, true]; // any reverts TS back to JS
+
+ids.push(6);
+ids.push('7') // ERROR: Argument of  type 'string' is not assignable to parameter of type  'numeber'
+
+
